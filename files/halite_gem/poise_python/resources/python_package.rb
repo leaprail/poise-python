caa84467b0d0cfa2284bc38c2fc8b9df34727d0e
Shayan Zadeh <shayan@zadeh.us>
2020-12-23 16:55:32 -0600

    InstallationCandidate.location is not .link

--- files/halite_gem/poise_python/resources/python_package.rb
+++ files/halite_gem/poise_python/resources/python_package.rb
@@ -102,7 +102,7 @@ with cmd._build_session(options) as session:
     req = install_req_from_line(arg)
     found = finder.find_requirement(req, True)
     all_candidates = find_all(req.name)
-    candidate = [c for c in all_candidates if c.location == found]
+    candidate = [c for c in all_candidates if c.link == found]
     if candidate:
       packages[candidate[0].project.lower()] = str(candidate[0].version)
 json.dump(packages, sys.stdout)
